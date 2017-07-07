a9f9f18a041b82043418328b853fd01d21af6ed5
Enrique Sanz <enrique.sanz@deusto.es>
2017-07-07 13:36:27 +0200

    Revamped categories list

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -12,6 +12,25 @@ class NavbarController {
     this.isAdmin = Auth.isAdmin;
     this.getCurrentUser = Auth.getCurrentUser;
 
+    this.detectIE = function detectIE() {
+          var ua = window.navigator.userAgent;
+
+          var msie = ua.indexOf('MSIE ');
+          if (msie > 0) {
+              // IE 10 or older => return version number
+              return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+          }
+
+          var trident = ua.indexOf('Trident/');
+          if (trident > 0) {
+              // IE 11 => return version number
+              var rv = ua.indexOf('rv:');
+              return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+          }
+          // other browser
+          return false;
+      };
+
     this.search = function(keyword) {
       $state.go('main', {keyword: keyword}, {reload: true});
     };
