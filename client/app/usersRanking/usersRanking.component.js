55656d3952d32955f3450b5343e66f64f85a858e
Enrique Sanz <enriquesanz@gmail.com>
2018-04-19 13:38:47 +0000

    Ranking added

--- client/app/usersRanking/usersRanking.component.js
+++ client/app/usersRanking/usersRanking.component.js
@@ -16,12 +16,55 @@ angular.module('paizaqaApp')
     $scope.noAdminMessage = "Admin Only Zone";
 
     $http.get(appConfig.path + '/api/users', {params: {query: query}}).success(function(users) {
+     
+      var apiGame = appConfig.path+'/api/users/';
+      $http.get(apiGame + 'getscorelist/full').success(function(scoreList) {
+
+        for(var i = 0; i < users.length; i++) {
+          for (let index = 0; index < scoreList.board.length; index++) {
+            if (users[i]._id === scoreList.board[index].playerId) {
+              users[i].score = scoreList.board[index].score;
+              // Admin user: color: #438276
+              // Bronze User: #CB5C0D 0-99
+              // Silver user: #C0C0C0 100-399
+              // Gold user: #D4AF37 >400
+              if (scoreList.board[index].score == 0)
+              {
+                users[i].medal = "#009EE0";
+              }
+              else if (scoreList.board[index].score > 0 && scoreList.board[index].score <= 99)
+              {
+                users[i].medal = "#CB5C0D";
+              }
+              else if (scoreList.board[index].score >= 100 && scoreList.board[index].score <= 399)
+              {
+                users[i].medal = "#C0C0C0";
+              }
+              else if (scoreList.board[index].score >= 400)
+              {
+                users[i].medal = "#D4AF37";
+              }
+            }
+          }
+          if (typeof users[i].score == 'undefined') {
+            users[i].score = 0;
+            users[i].medal = "#009EE0";
+          }
+        }
+        users.sort(function(a,b) {return (a.score > b.score) ? -1 : ((b.score > a.score) ? 1 : 0);});
+      });
+    
       $scope.users = users;
+
       if($scope.users.length < 20){
         $scope.noMoreData = true;
       }
       $scope.busy = false;
     });
+
+    
+
+
 
     $scope.nextPage = function(){
       if($scope.busy){ return; }
