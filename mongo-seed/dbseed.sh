594b4f1ecc2b3cb748aaa6d8b6e0b0b5ea0e2430
Enrique Sanz <enriquesanz@gmail.com>
2018-05-31 11:12:59 +0200

    Added users seed

--- mongo-seed/dbseed.sh
+++ mongo-seed/dbseed.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
 mongoimport --host mongodb --db paizaqa-dev --collection categories --type json --jsonArray --file /mongo-seed/categories.json
+mongoimport --host mongodb --db paizaqa-dev --collection users --type json --jsonArray --file /mongo-seed/users.json
