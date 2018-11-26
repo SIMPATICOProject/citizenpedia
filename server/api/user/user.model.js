545de687dcc92f65b8409e3360cc75d9627e5516
Enrique Sanz <enriquesanz@gmail.com>
2018-11-26 14:20:46 +0000

    Again, Changed crypto return

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -213,11 +213,13 @@ UserSchema.methods = {
     var salt = new Buffer(this.salt, 'base64');
 
     if (!callback) {
-      return crypto.pbkdf2Sync(password, salt, defaultIterations, defaultKeyLength)
+      //return crypto.pbkdf2Sync(password, salt, defaultIterations, defaultKeyLength)
+      return crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512')
                    .toString('base64');
     }
 
-    return crypto.pbkdf2(password, salt, defaultIterations, defaultKeyLength, (err, key) => {
+    //return crypto.pbkdf2(password, salt, defaultIterations, defaultKeyLength, (err, key) => {
+    return crypto.pbkdf2(password, salt, 10000, 64, 'sha512', (err, key) => {
       if (err) {
         callback(err);
       } else {
